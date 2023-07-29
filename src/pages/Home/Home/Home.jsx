29cb9abb2f3ca4327c4a7067e1045fa07343e826
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-29 10:29:10 +0600

    footer

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,12 @@
+import Footer from "../../Shared/Footer/Footer";
 import Navbar from "../../Shared/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
+      
+      <Footer></Footer>
     </div>
   );
 };
