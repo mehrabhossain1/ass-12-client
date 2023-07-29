cda2415f8b493cf1dc473b26ba24068926f4bc09
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-30 02:53:02 +0600

    popular instructors

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,13 +1,15 @@
 import Container from "../../Shared/Container/Container";
 import Banner from "../Banner/Banner";
 import Classes from "../Classes/Classes";
+import PopularInstructors from "../PopularInstructors/PopularInstructors";
 
 const Home = () => {
   return (
     <div>
       <Container>
         <Banner></Banner>
-        <Classes></Classes>
+        <Classes></Classes> 
+        <PopularInstructors></PopularInstructors>
       </Container>
     </div>
   );
---
+++ src/pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -0,0 +1,11 @@
+import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
+
+const PopularInstructors = () => {
+  return (
+    <div>
+      <SectionTitle heading='Popular Instructors'></SectionTitle>
+    </div>
+  );
+};
+
+export default PopularInstructors;
