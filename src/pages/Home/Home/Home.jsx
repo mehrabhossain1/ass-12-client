6eeb34ffbb10e4610d017061c8152625e5b8dd8b
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-29 10:01:12 +0600

    navbar logo

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../Shared/Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import Container from "../Container/Container";
+import Logo from "./Logo";
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full bg-white z-10 shadow-md'>
+      <div className='py-4 border-b-[1px]'>
+        <Container>
+          <div className='flex flex-row  items-center justify-between gap-3 md:gap-0'>
+            <Logo />
+            <div>links</div>
+            <div>user</div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
