62be9558869d0c2e88635ff58fd5818a9f6d37e9
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-31 19:59:22 +0600

    instructors page

---
+++ src/pages/Instructors/Instructors.jsx
@@ -0,0 +1,50 @@
+import Container from "../Shared/Container/Container";
+import { useQuery } from "@tanstack/react-query";
+
+const Instructors = () => {
+  const { data: instructors = [] } = useQuery(["instructors"], async () => {
+    const res = await fetch("http://localhost:5000/instructor");
+    return res.json();
+  });
+
+  console.log(instructors);
+
+  return (
+    <Container>
+      <div className='bg-gray-100'>
+        <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+          <div className='mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32'>
+            <h2 className='text-3xl font-bold text-gray-900 text-center'>
+              Instructors
+            </h2>
+
+            <div className='m-6 p-10 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0'>
+              {instructors.map((callout) => (
+                <div key={callout.name} className='group relative'>
+                  <div className='relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64'>
+                    <img
+                      src={callout.image}
+                      alt=''
+                      className='h-full w-full object-cover object-center'
+                    />
+                  </div>
+                  <h3 className='mt-6 text-3xl text-gray-500'>
+                    <a href=''>
+                      <span className='absolute inset-0' />
+                      {callout.name}
+                    </a>
+                  </h3>
+                  <p className='text-base font-semibold text-gray-900 pb-10'>
+                    {callout.email}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Instructors;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
+import Instructors from "../pages/Instructors/Instructors";
 
 export const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ export const router = createBrowserRouter([
       {
         path: "signup",
         element: <SignUp></SignUp>,
+      },
+      {
+        path: "instructors",
+        element: <Instructors></Instructors>,
       },
     ],
   },
