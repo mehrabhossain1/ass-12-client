dd688e2e5c23a5b956b0fa0d0035d456c647f9e1
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-08-05 17:58:41 +0600

    social login

--- src/pages/Shared/SocialLogin/SocialLogin.jsx
+++ src/pages/Shared/SocialLogin/SocialLogin.jsx
@@ -5,20 +5,31 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../Providers/AuthProvider";
 
 const SocialLogin = () => {
-  const { setLoading, signInWithGoogle } = useContext(AuthContext);
+  const { signInWithGoogle } = useContext(AuthContext);
   const from = location.state?.from?.pathname || "/";
   const navigate = useNavigate();
   // handle google sign in
   const handleGoogleSignIn = () => {
-    signInWithGoogle()
-      .then((result) => {
-        console.log(result.user);
-        navigate(from, { replace: true });
+    signInWithGoogle().then((result) => {
+      const loggedInUser = result.user;
+      console.log(loggedInUser);
+
+      const savedUser = {
+        name: loggedInUser.displayName,
+        email: loggedInUser.email,
+      };
+      fetch("http://localhost:5000/users", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(savedUser),
       })
-      .catch((err) => {
-        setLoading(false);
-        console.log(err.message);
-      });
+        .then((res) => res.json())
+        .then(() => {
+          navigate(from, { replace: true });
+        });
+    });
   };
 
   return (
