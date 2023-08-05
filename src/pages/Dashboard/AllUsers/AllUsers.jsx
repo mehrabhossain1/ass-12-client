4b929e745057d6ea8ef7f72d6f0b880245360c7a
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-08-05 20:39:00 +0600

    show all users

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,11 +1,40 @@
-
+import { useQuery } from "@tanstack/react-query";
 
 const AllUsers = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { data: users = [] } = useQuery(["users"], async () => {
+    const res = await fetch("http://localhost:5000/users");
+    return res.json();
+  });
+
+  return (
+    <div>
+      <h3 className='text-3xl font-semibold'>Total Users: {users.length}</h3>
+
+      <div className='overflow-x-auto'>
+        <table className='table table-zebra'>
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllUsers;
