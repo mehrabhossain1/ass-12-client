8f8484650fb87839ba84bdb41be10cd18b73deb5
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-08-05 10:49:24 +0600

    error message

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,8 +1,10 @@
 import { useStripe, CardElement, useElements } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const [cardError, setCardError] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -10,36 +12,56 @@ const CheckoutForm = () => {
     if (!stripe || !elements) {
       return;
     }
-    
+
     const card = elements.getElement(CardElement);
 
     if (card == null) {
       return;
     }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      setCardError(error.message);
+    } else {
+      setCardError("");
+      console.log(paymentMethod);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
+    <>
+      {" "}
+      <form className='w-2/3 m-10' onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
               },
             },
-            invalid: {
-              color: "#9e2146",
-            },
-          },
-        }}
-      />
-      <button type='submit' disabled={!stripe}>
-        Pay
-      </button>
-    </form>
+          }}
+        />
+        <button
+          className='btn btn-outline btn-circle btn-warning mt-4'
+          type='submit'
+          disabled={!stripe}
+        >
+          Pay
+        </button>
+      </form>
+      {cardError && <p className='text-error ml-10'>{cardError}</p>}
+    </>
   );
 };
 
