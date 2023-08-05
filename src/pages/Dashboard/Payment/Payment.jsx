992f9200aac729359a9035fdf2b2dfdf07a5748c
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-08-05 15:38:45 +0600

    checkout form

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -2,17 +2,23 @@ import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
 import CheckoutForm from "./CheckoutForm";
 import { Elements } from "@stripe/react-stripe-js";
+import useSelectedClass from "../../../hooks/useSelectedClass";
 
 // TODO: provide publishable key
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
 const Payment = () => {
+  const [selectedClass] = useSelectedClass();
+  const total = selectedClass.reduce((sum, item) => sum + item.price, 0);
+  const price = parseFloat(total.toFixed(2));
+
   return (
     <div>
       <SectionTitle heading='Payment'></SectionTitle>
       <h2 className='text-3xl'>Payment</h2>
+
       <Elements stripe={stripePromise}>
-        <CheckoutForm></CheckoutForm>
+        <CheckoutForm price={price}></CheckoutForm>
       </Elements>
     </div>
   );
