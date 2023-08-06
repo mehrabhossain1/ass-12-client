06d690b3e88d1c9e4c1875c6c1a2568d5d0f7e99
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-08-06 16:27:46 +0600

    again installed node modules

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -10,11 +10,12 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { app } from "../firebase/firebase.init";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider()
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
@@ -31,9 +32,9 @@ const AuthProvider = ({ children }) => {
   };
 
   const signInWithGoogle = () => {
-    setLoading(true)
-    return signInWithPopup(auth, googleProvider)
-  }
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
 
   const logOut = () => {
     setLoading(true);
@@ -51,6 +52,16 @@ const AuthProvider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       console.log("current user", currentUser);
+
+      // get and set token
+      if (currentUser) {
+        axios
+          .post("http://localhost:5000/jwt", { email: currentUser.email })
+          .then((data) => {
+            console.log(data);
+          });
+      }
+
       setLoading(false);
     });
     return () => {
@@ -66,7 +77,7 @@ const AuthProvider = ({ children }) => {
     signIn,
     signInWithGoogle,
     logOut,
-    updateUserProfile
+    updateUserProfile,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
